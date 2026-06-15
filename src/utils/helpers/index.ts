06635d80a6b9ff2e3952f55531be156f676e2220
Ranjith <ranjithsam21@gmail.com>
2026-06-15 12:40:28 +0530

    modified:   src/utils/helpers/index.ts

--- src/utils/helpers/index.ts
+++ src/utils/helpers/index.ts
@@ -1,1 +1,9 @@
-export const getEnvVariable = (name: string) => import.meta.env[name];
+export const getEnvVariable = (name: string) => {
+  if (name === "VITE_REACT_APP_BASE_URL") {
+    return import.meta.env.VITE_REACT_APP_BASE_URL;
+  }
+  if (name === "VITE_REACT_APP_ADMIN_EMAIL") {
+    return import.meta.env.VITE_REACT_APP_ADMIN_EMAIL;
+  }
+  return undefined;
+};
